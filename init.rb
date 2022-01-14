7274ec0ab96b0e0a4c48c6baf87a6d81fc2731d9
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2022-01-15 00:27:46 +0300

    Reflect

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require_relative "string"
-require_relative "item_container"
 require_relative "item"
+require_relative "item_container"
 require_relative "real_item"
 require_relative "virtual_item"
 require_relative "antique_item"
@@ -12,3 +12,10 @@ require_relative "cart"
 @items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
 @items << RealItem.new({:price => 101, :weight => 100, :name => "kettle"})
 @items << RealItem.new({:price => 101, :weight => 100, :name => "dishwasher"})
+
+cart = Cart.new("roman")
+cart.add_item RealItem.new({:price => 121, :weight => 100, :name => "car"})
+cart.add_item RealItem.new({:price => 151, :weight => 100, :name => "car"})
+cart.add_item RealItem.new({:price => 101, :weight => 100, :name => "kettle"})
+p cart.all_cars
+p cart.all_kettle
