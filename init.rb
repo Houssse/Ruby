e2446d80c4082993c4c21764a99587a129aa5fd0
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2022-01-15 01:43:06 +0300

    Singl

--- init.rb
+++ init.rb
@@ -1,12 +1,6 @@
-require_relative "string"
-require_relative "item"
-require_relative "item_container"
-require_relative "real_item"
-require_relative "virtual_item"
-require_relative "antique_item"
-require_relative "order"
-require_relative "cart"
+require_relative "store_application"
 
+StoreApplication.new
 
 @items = []
 @items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
