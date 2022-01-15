da077958f86f4d5ea4ccacfe7128fdf85b0a4589
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2022-01-15 03:06:30 +0300

    Dsl

--- init.rb
+++ init.rb
@@ -1,6 +1,23 @@
 require_relative "store_application"
 
-StoreApplication.new
+StoreApplication.config do |app|
+
+  app.name        = "My Store"
+  app.environment = :production
+  
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+    admin.login = "login"
+    admin.send_info_emails_on :mondays
+  end
+
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
+
 
 @items = []
 @items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
@@ -11,5 +28,5 @@ cart = Cart.new("roman")
 cart.add_item RealItem.new({:price => 121, :weight => 100, :name => "car"})
 cart.add_item RealItem.new({:price => 151, :weight => 100, :name => "car"})
 cart.add_item RealItem.new({:price => 101, :weight => 100, :name => "kettle"})
-p cart.all_cars
-p cart.all_kettle
+#p cart.all_cars
+#p cart.all_kettle
